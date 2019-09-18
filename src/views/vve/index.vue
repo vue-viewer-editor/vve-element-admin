414811e74e7f614005f0a96215f7e6053e6d2d79
nianqin <nianqin.vip@gmail.com>
2019-09-18 14:55:45 +0800

    支持国际化

--- src/views/vve/index.vue
+++ src/views/vve/index.vue
@@ -1,11 +1,8 @@
 <template>
   <div>
-    <el-checkbox>是否选中</el-checkbox>
-    <div class="__p_F0_u_4">
-      <span>内容：</span>
-      <el-input placeholder="请输入内容" type="text" size="small" class="__p_F0_u_6"></el-input>
-    </div>
-    <el-button type="primary" size="small" class="__p_F0_u_7">保存</el-button>
+    <p>我是一段文本</p><img src="http://vve.qiniu.qjzd.net/FhoxiAgrwxt4pfXTbbMTgB9j7hpy" class="__p_7Z_u_3" />
+    <el-button type="primary">{{ $t('按钮3333323232323') }}</el-button><span>我是文本31423423</span>
+    <el-input :placeholder="$t('请输入内容')" type="text"></el-input>
   </div>
 </template>
 <script>
@@ -13,16 +10,7 @@ export default {}
 </script>
 
 <style scoped>
-.__p_F0_u_6 {
-  width: 210px;
-}
-
-.__p_F0_u_4 {
-  margin-top: 10px;
-}
-
-.__p_F0_u_7 {
-  width: 100px;
-  margin-top: 20px;
-}
+  .__p_7Z_u_3 {
+    vertical-align: bottom;
+  }
 </style>
