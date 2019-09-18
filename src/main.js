6ec62153be95376744ffdb08b40be1dcea8a362b
nianqin <nianqin.vip@gmail.com>
2019-09-18 14:50:24 +0800

    引入国际化

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import locale from 'element-ui/lib/locale/lang/zh-CN' // lang i18n
 
 import '@/styles/index.scss' // global css
 
@@ -12,6 +11,7 @@ import App from './App'
 import store from './store'
 import router from './router'
 
+import i18n from './lang' // internationalization
 import '@/icons' // icon
 import '@/permission' // permission control
 
@@ -29,7 +29,9 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 // set ElementUI lang to EN
-Vue.use(ElementUI, { locale })
+Vue.use(ElementUI, {
+  i18n: (key, value) => i18n.t(key, value)
+})
 
 Vue.config.productionTip = false
 
@@ -37,5 +39,6 @@ new Vue({
   el: '#app',
   router,
   store,
+  i18n,
   render: h => h(App)
 })
