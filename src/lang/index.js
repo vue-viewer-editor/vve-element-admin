a95eeee278e09ef6439193f9cea0c70ff7da71f8
nianqin <nianqin.vip@gmail.com>
2019-09-26 10:23:40 +0800

    使用json作为语言配置

--- src/lang/index.js
+++ src/lang/index.js
@@ -3,8 +3,8 @@ import VueI18n from 'vue-i18n'
 import Cookies from 'js-cookie'
 import elementEnLocale from 'element-ui/lib/locale/lang/en' // element-ui lang
 import elementZhLocale from 'element-ui/lib/locale/lang/zh-CN'// element-ui lang
-import enLocale from './en'
-import zhLocale from './zh'
+import enLocale from './en.json'
+import zhLocale from './zh.json'
 
 Vue.use(VueI18n)
 
