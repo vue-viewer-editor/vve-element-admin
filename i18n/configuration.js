c07e9388b72afeeec21a13e65946349396f1c694
nianqin <bu__dong@163.com>
2019-10-03 17:13:09 +0800

    update

--- i18n/configuration.js
+++ i18n/configuration.js
@@ -1,32 +1,7 @@
 const buildDebug = require("debug");
 const path = require("path");
-const fs = require("fs");
 
 const debug = buildDebug("files:configuration");
-
-const CONFIG_JS_FILENAME = "vve-i18n-cli.config.js";
-
-function findConfigUpwards() {
-  let dirname = rootDir;
-  while (true) {
-    if (fs.existsSync(path.join(dirname, CONFIG_JS_FILENAME))) {
-      return dirname;
-    }
-    const nextDir = path.dirname(dirname);
-    if (dirname === nextDir) break;
-    dirname = nextDir;
-  }
-  return null;
-}
-
-function findConfig (dirname) {
-  const filepath = path.resolve(dirname, CONFIG_JS_FILENAME);
-  const conf = readConfig(filepath);
-  if (conf) {
-    debug("Found root config %o in %o.", CONFIG_JS_FILENAME, dirname);
-  }
-  return conf;
-}
 
 function loadConfig (filepath) {
   try {
@@ -57,7 +32,6 @@ function readConfig(filepath) {
 }
 
 module.exports = {
-  findConfigUpwards,
   loadConfig,
   readConfig,
 }
