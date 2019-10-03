6e731703a2b4646c3c87604b3287d21de677278d
nianqin <bu__dong@163.com>
2019-10-03 23:41:02 +0800

    添加vve-i18n-cli，支持抽取国际化文本，生成国际化文本文件

--- src/views/vve/index.vue
+++ src/views/vve/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <p>我是一段文本</p><img src="http://vve.qiniu.qjzd.net/FhoxiAgrwxt4pfXTbbMTgB9j7hpy" class="__p_7Z_u_3" />
-    <el-button type="primary">{{ $t('按钮3333323232323') }}</el-button><span>我是文本31423423</span>
+    <el-button type="primary">{{ $t('提交') }}</el-button>
     <el-input :placeholder="$t('请输入内容')" type="text"></el-input>
   </div>
 </template>
